01f149b541d56a2b0681788c35df20c7259e2f39
Mike <15673068+mikemherron@users.noreply.github.com>
2023-03-16 12:00:04 +0000

    Minor tidy ups

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,59 +8,65 @@ public class PlayerHealthUpdatedEvent : UnityEvent<int> { }
 
 public class PlayerController : MonoBehaviour
 {
-    public PlayerHealthUpdatedEvent onPlayerHealthUpdated;
+  public PlayerHealthUpdatedEvent onPlayerHealthUpdated;
 
-    public GameObject bulletPrefab;
-    public int health = 10;
-    public float moveSpeed = 0f;
-    public Rigidbody2D rigidBody;
-    public Vector2 movement;
-    public Animator animator;
-    public AudioSource hitSound;
-    
-    void Start()
+  public GameObject bulletPrefab;
+  public Rigidbody2D rigidBody;
+  public Vector2 movement;
+  public Animator animator;
+  public AudioSource hitSound;
+
+  public int health = 10;
+  public float moveSpeed = 0f;
+
+  void Start()
+  {
+    if (onPlayerHealthUpdated == null)
     {
-        if(onPlayerHealthUpdated == null) {
-            onPlayerHealthUpdated = new PlayerHealthUpdatedEvent();
+      onPlayerHealthUpdated = new PlayerHealthUpdatedEvent();
+    }
+  }
+
+  void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+      Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+      direction.Normalize();
+      GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+      BulletController controller = bullet.GetComponent<BulletController>();
+      controller.direction = direction;
+      controller.firedBy = gameObject;
+      Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+    }
+
+    movement.x = Input.GetAxisRaw("Horizontal");
+    movement.y = Input.GetAxisRaw("Vertical");
+    animator.SetFloat("Horizontal", movement.x);
+    animator.SetFloat("Vertical", movement.y);
+    animator.SetFloat("Speed", movement.sqrMagnitude);
+  }
+
+  void FixedUpdate()
+  {
+    rigidBody.MovePosition(rigidBody.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+  }
+
+  void OnCollisionEnter2D(Collision2D collision)
+  {
+    if (collision.gameObject.tag == "Bullet")
+    {
+      BulletController controller = collision.gameObject.GetComponent<BulletController>();
+      if (controller.firedBy.tag == "Turret")
+      {
+        Destroy(collision.gameObject);
+        health--;
+        onPlayerHealthUpdated.Invoke(health);
+        if (health > 0)
+        {
+          hitSound.Play();
         }
+      }
     }
-
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0)) {
-            Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            direction.Normalize();
-            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
-            BulletController controller = bullet.GetComponent<BulletController>();
-            controller.direction = direction;
-            controller.firedBy = gameObject;
-            Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), GetComponent<Collider2D>());
-        }
-
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
-    }
-
-    void FixedUpdate() 
-    {
-        rigidBody.MovePosition(rigidBody.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Bullet") {
-            BulletController controller = collision.gameObject.GetComponent<BulletController>();
-            if (controller.firedBy.tag == "Turret") {
-                Destroy(collision.gameObject);
-                health--;
-                onPlayerHealthUpdated.Invoke(health);
-                if(health > 0) {
-                    hitSound.Play();
-                }
-            }
-        }
-    }
+  }
 }
--- Assets/Scripts/TurretController.cs
+++ Assets/Scripts/TurretController.cs
@@ -4,49 +4,47 @@ using UnityEngine;
 
 public class TurretController : MonoBehaviour
 {
-    public GameObject bulletPrefab;
+  public GameObject bulletPrefab;
+  public GameObject fireTarget;
+  public float fireDelay = 0f;
+  public AudioSource hitSound;
 
-    public GameObject fireTarget;
+  private float timeToNextFire = 0f;
 
-    public float fireDelay = 0f;
+  void Start()
+  {
+    timeToNextFire = fireDelay * Random.value;
+  }
 
-    public AudioSource hitSound;
-
-    private float timeToNextFire = 0f;
-    
-    void Start()
+  void Update()
+  {
+    timeToNextFire -= Time.deltaTime;
+    if (timeToNextFire <= 0f)
     {
-        timeToNextFire = fireDelay * Random.value;
-    }
+      timeToNextFire = fireDelay;
+      GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+      Vector2 direction = fireTarget.transform.position - transform.position;
+      direction.Normalize();
 
-    // Update is called once per frame
-    void Update()
+      BulletController controller = bullet.GetComponent<BulletController>();
+      controller.direction = direction;
+      controller.speed *= (float)0.5;
+      controller.firedBy = gameObject;
+
+      bullet.GetComponent<SpriteRenderer>().color = Color.red;
+    }
+  }
+
+  void OnTriggerEnter2D(Collider2D collision)
+  {
+    if (collision.gameObject.tag == "Bullet")
     {
-        timeToNextFire -= Time.deltaTime;
-        if (timeToNextFire <= 0f) {
-            timeToNextFire = fireDelay;
-            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
-            Vector2 direction = fireTarget.transform.position - transform.position;
-            direction.Normalize();
-
-            BulletController controller = bullet.GetComponent<BulletController>();
-            controller.direction = direction;
-            controller.speed *= (float)0.5;
-            controller.firedBy = gameObject;
-            
-            bullet.GetComponent<SpriteRenderer>().color = Color.red;
-        }        
+      BulletController bullet = collision.gameObject.GetComponent<BulletController>();
+      if (bullet.firedBy.tag == "Player")
+      {
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
+      }
     }
-
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("Turret hit by bullet");
-        if (collision.gameObject.tag == "Bullet") {
-            BulletController bullet = collision.gameObject.GetComponent<BulletController>();
-            if (bullet.firedBy.tag == "Player") {
-                Destroy(collision.gameObject);
-                Destroy(gameObject);
-            }
-        }
-    }
+  }
 }
